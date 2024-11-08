6097b1f6194406701d1463c0a438723fae7ea110
Lenz60 <raflyandrian@gmail.com>
2024-11-08 15:56:29 +0700

    feat(MyContext): Added new Context
    Added new context, to be able to create new models

--- JobseekBerca/Context/MyContext.cs
+++ JobseekBerca/Context/MyContext.cs
@@ -8,5 +8,10 @@ namespace JobseekBerca.Context
         public MyContext(DbContextOptions<MyContext> options) : base(options) { }
         public DbSet<Users> Users { get; set; }
         public DbSet<Roles> Roles { get; set; }
+        public DbSet<Profiles> Profiles { get; set; }
+        public DbSet<Experiences> Experiences { get; set; }
+        public DbSet<Educations> Educations { get; set; }
+        public DbSet<Skills> Skills { get; set; }
+        public DbSet<Certificates> Certificates { get; set; }
     }
 }
