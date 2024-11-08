fc7aea2250e9550bcbfe4f93a779b5f9249db662
Lenz60 <raflyandrian@gmail.com>
2024-11-08 15:55:45 +0700

    feat(Models): Added new models
    Added Profiles, Experiences, Educations, Certificates, Skills models \n

---
+++ JobseekBerca/Models/Experiences.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace JobseekBerca.Models
+{
+    public class Experiences
+    {
+        [Key]
+        public int experienceId { get; set; }
+        public string? position { get; set; }
+        public string? company { get; set; }
+        public string? description { get; set; }
+        public string? jobTypes { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+        [JsonIgnore]
+        public virtual Profiles? Profile { get; set; }
+        [ForeignKey("Profile"), Required]
+        public string userId { get; set; }
+    }
+}
---
+++ JobseekBerca/Models/Profiles.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace JobseekBerca.Models
+{
+    public enum Gender
+    {
+        Laki_laki,
+        Perempuan
+    }
+    public class Profiles
+    {
+        [Key, ForeignKey("Users")]
+        public string userId { get; set; }
+        public string full_name { get; set; }
+        public string summary { get; set; }
+        public Gender gender { get; set; }
+        public string address { get; set; }
+        public DateTime birthdate { get; set; }
+        [JsonIgnore]
+        public virtual Users? Users { get; set; }
+
+    }
+}
