49d77e0eaae5f031c14b2f1948a04d1280e75d10
Lenz60 <raflyandrian@gmail.com>
2024-11-08 15:57:09 +0700

    feat(Migration): Added new migration
    Migration file for the Profiles and its children

--- JobseekBerca/Migrations/20241108084405_add profile,experience,education,skill,certificate.Designer.cs
+++ JobseekBerca/Migrations/20241108084405_add profile,experience,education,skill,certificate.Designer.cs
@@ -4,6 +4,7 @@ using JobseekBerca.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JobseekBerca.Migrations
 {
     [DbContext(typeof(MyContext))]
-    partial class MyContextModelSnapshot : ModelSnapshot
+    [Migration("20241108084405_add profile,experience,education,skill,certificate")]
+    partial class addprofileexperienceeducationskillcertificate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
