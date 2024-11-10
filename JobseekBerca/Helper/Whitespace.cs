0f28fc627f124a3c65ebbe73b2b83b7324b79d93
Rafly Andrian <raflyandrian@gmail.com>
2024-11-11 00:36:06 +0700

    feat(Helper): Added new helper
    Added new helper class to check if the parameter given is null or

---
+++ JobseekBerca/Helper/Whitespace.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace JobseekBerca.Helper
+{
+    public class Whitespace
+    {
+        public static bool HasNullOrEmptyStringProperties<T>(T model, out string propertyName)
+        {
+            propertyName = null;
+            foreach (PropertyInfo property in typeof(T).GetProperties())
+            {
+                if (property.PropertyType == typeof(string))
+                {
+                    var value = (string)property.GetValue(model);
+                    if (string.IsNullOrEmpty(value))
+                    {
+                        propertyName = property.Name;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
