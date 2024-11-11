3ffb705221d775d6a2cbb8eb2a30fb530864937f
Lenz60 <raflyandrian@gmail.com>
2024-11-11 21:59:31 +0700

    feat(HttpResponseExceptionHelper): Added new exception helper
    Added new helper, the helper is used for throwing error with http status

---
+++ JobseekBerca/Helper/HttpResponseExceptionHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace JobseekBerca.Helper
+{
+    public class HttpResponseExceptionHelper :Exception
+    {
+        public int StatusCode { get; }
+
+        public HttpResponseExceptionHelper(int statusCode, string message) : base(message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
