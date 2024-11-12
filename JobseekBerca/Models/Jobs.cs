5325d45b3d1eee058e46783768a49178528d0ab8
Lenz60 <raflyandrian@gmail.com>
2024-11-12 18:27:12 +0700

    fix(job): added new field
    Added new field on Jobs called type and requirement

--- JobseekBerca/Models/Jobs.cs
+++ JobseekBerca/Models/Jobs.cs
@@ -10,7 +10,9 @@ namespace JobseekBerca.Models
         public string? jobId { get; set; }
         public string? title { get; set; }
         public string? description { get; set; }
+        public string? type { get; set; }
         public string? salary { get; set; }
+        public string? requirement { get; set; } 
         public string? location { get; set; }
 
 
