227ebb919e5df996f09c0f6b7e7cef3d18e054f2
Lenz60 <raflyandrian@gmail.com>
2024-11-12 18:33:27 +0700

    feat(applicationvm): application vm
    Application VM for getting specific user applications

---
+++ JobseekBerca/ViewModels/ApplicationsVM.cs
@@ -0,0 +1,17 @@
+﻿using JobseekBerca.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JobseekBerca.ViewModels
+{
+    public class ApplicationsVM
+    {
+        public class UserApplicationsVM
+        {
+            public string jobTitle { get; set; }
+            public string jobType { get; set; }
+            public string jobLocation { get; set; }
+            public string jobRequirement { get; set; }
+            public string jobSalary { get; set; }
+        }
+    }
+}
