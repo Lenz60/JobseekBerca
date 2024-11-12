422f63bbedf91a6783e03a262668aa138375e878
Lenz60 <raflyandrian@gmail.com>
2024-11-12 18:35:25 +0700

    fix(whitespace helper): added new nullable parameter
    Now can passing a hash map to make specific field is nullable

--- JobseekBerca/Helper/Whitespace.cs
+++ JobseekBerca/Helper/Whitespace.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -6,7 +7,7 @@ namespace JobseekBerca.Helper
 {
     public class Whitespace
     {
-        public static bool HasNullOrEmptyStringProperties<T>(T model, out string propertyName)
+        public static bool HasNullOrEmptyStringProperties<T>(T model, out string propertyName, HashSet<string> nullableFields = null)
         {
             propertyName = null;
             foreach (PropertyInfo property in typeof(T).GetProperties())
@@ -14,7 +15,7 @@ namespace JobseekBerca.Helper
                 if (property.PropertyType == typeof(string))
                 {
                     var value = (string)property.GetValue(model);
-                    if (string.IsNullOrEmpty(value))
+                    if (string.IsNullOrEmpty(value) && (nullableFields == null || !nullableFields.Contains(property.Name)))
                     {
                         propertyName = property.Name;
                         return true;
