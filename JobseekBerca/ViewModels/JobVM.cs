f1ef38b9e1851c726ab3d7184f6b1ded80262581
Lenz60 <raflyandrian@gmail.com>
2024-11-13 15:17:56 +0700

    fix(delete job): change parameter on delete job
    Change delete job request from parameter to body using VM

---
+++ JobseekBerca/ViewModels/JobVM.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace JobseekBerca.ViewModels
+{
+    public class JobVM
+    {
+        public class DeleteJobVM
+        {
+            public string userId { get; set; }
+            public string jobId { get; set; }
+        }
+    }
+}
