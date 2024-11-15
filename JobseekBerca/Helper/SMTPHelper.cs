c0c8fa0a51d4fbf9bdb66d43da6b138751a08fd1
Lenz60 <raflyandrian@gmail.com>
2024-11-15 19:02:44 +0700

    feat(smtp): added smtp
    Added SMTP for sending password to email user

---
+++ JobseekBerca/Helper/SMTPHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Options;
+using System.Net.Mail;
+using System.Net;
+using JobseekBerca.Helper.Interface;
+
+namespace JobseekBerca.Helper
+{
+    public class SMTPHelper : ISMTPHelper
+    {
+        private readonly SmtpSettings _smtpSettings;
+
+        public SMTPHelper(IOptions<SmtpSettings> smtpSettings)
+        {
+            _smtpSettings = smtpSettings.Value;
+        }
+
+        public void SendEmail(string toEmail, string subject, string body)
+        {
+            var smtpClient = new SmtpClient(_smtpSettings.Server)
+            {
+                Port = _smtpSettings.Port,
+                Credentials = new NetworkCredential(_smtpSettings.Username, _smtpSettings.Password),
+                EnableSsl = true,
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(_smtpSettings.SenderEmail, _smtpSettings.SenderName),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true,
+            };
+
+            mailMessage.To.Add(toEmail);
+
+            smtpClient.Send(mailMessage);
+        }
+    }
+}
